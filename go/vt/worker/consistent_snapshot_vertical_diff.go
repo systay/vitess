ee77b09e3c99b97f086b03d5f326329e7ed076b0
Andres Taylor <antaylor@squareup.com>
2019-03-07 15:12:05 +0100

    Comments on how to do consistent vertical split diff

--- go/vt/worker/consistent_snapshot_vertical_diff.go
+++ go/vt/worker/consistent_snapshot_vertical_diff.go
@@ -30,6 +30,11 @@ import (
 type ConsistentSnapshotDiffer struct {
   logger  logutil.Logger
   cleaner *wrangler.Cleaner
+
+  // written during StabilizeSourceAndDestination
+  lastPos         string // contains the GTID position for the source
+  srcTransactions []int64
+  dstTransactions []int64
 }
 
 func NewConsistentSnapshotDiffer(logger logutil.Logger) Differ {
@@ -41,25 +46,53 @@ func NewConsistentSnapshotDiffer(logger logutil.Logger) Differ {
   return &x
 }
 
-func (*ConsistentSnapshotDiffer) StabilizeSourceAndDestination(ctx context.Context,
+// StabilizeSourceAndDestination phase:
+// 1 - ask the master of the destination shard to pause filtered replication
+//     (add a cleanup task to restart filtered replication on master)
+// 2 - create consistent snapshot read-only transactions on source tables
+//     and note the gtid we arrived at
+// 3 - ask the master of the destination shard to resume filtered replication
+//     up to the new list of positions, and return its binlog position.
+// 4 - wait until the destination tablet is equal or passed that master
+//     binlog position
+// 5 - create consistent snapshot read-only transactions on destination tables
+// At this point, all source and destination transactions should see the same data
+func (csd *ConsistentSnapshotDiffer) StabilizeSourceAndDestination(ctx context.Context,
   toposerver *topo.Server,
   tabletManagerClient tmclient.TabletManagerClient,
   shardInfo *topo.ShardInfo,
   sourceAlias *topodata.TabletAlias,
   destinationAlias *topodata.TabletAlias) error {
-  panic("implement me")
+
+  shortCtx, cancel := context.WithTimeout(ctx, *remoteActionsTimeout)
+  ti, err := toposerver.GetTablet(shortCtx, sourceAlias)
+  cancel()
+  if err != nil {
+    return vterrors.Wrapf(err, "trying to get tablet info for %v failed", sourceAlias)
+  }
+
+  sourceTransactions, gtid, err := CreateConsistentTransactions(ctx, ti, csd.logger, csd.cleaner, 8)
+  if err != nil {
+    return err
+  }
+  csd.lastPos = gtid
+  csd.srcTransactions = sourceTransactions
+  return nil
 }
 
-func (*ConsistentSnapshotDiffer) Diff(ctx context.Context,
+func (csd *ConsistentSnapshotDiffer) Diff(ctx context.Context,
   wr *wrangler.Wrangler,
   sourceAlias *topodata.TabletAlias,
   destinationAlias *topodata.TabletAlias,
   shardInfo *topo.ShardInfo,
   markAsWillFail func(rec concurrency.ErrorRecorder, err error),
   parallelDiffsCount int) error {
-  schemaInfo, err := SchemaDiff(ctx, wr, sourceAlias, destinationAlias, shardInfo, markAsWillFail)
+
+  _, err := SchemaDiff(ctx, wr, sourceAlias, destinationAlias, shardInfo, markAsWillFail)
   if err != nil {
     return vterrors.Wrapf(err, "schema diff failed")
   }
-  panic(schemaInfo)
+
+  panic("implement me")
 }
+
