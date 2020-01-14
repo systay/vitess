b2181badfae6b8842bc9ee3ab321c49d9f243690
Roni He <herongrong2011@gmail.com>
2020-01-13 20:34:44 -0800

    Support filtering for SHOW CHARSET
    right now the show character set are hardcoded to be filtered with where statement.
    since we don't really want to expand the results or forward the query to an underlying mysql because vitess only supports these 2 charsets.
    Ideally there should be a where modular to do the function.
    
    seperate show charset vs show charset in parser
    changed the executor accordingly
    use where_expression_opt in parser, change ast and exector accordingly
    change the parser for show charset to filter with where or like
    exec func done, need to add more test
    add err in func generateCharsetRows, added tests
    fix err bug in the func generateCharsetRow
    correct error type to vterror
    
    Signed-off-by: Roni He <herongrong2011@gmail.com>
    
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/sqlparser/parse_test.go
+++ go/vt/sqlparser/parse_test.go
@@ -1138,13 +1138,13 @@ var (
 		output: "show charset",
 	}, {
 		input:  "show character set like '%foo'",
-		output: "show charset",
+		output: "show charset like '%foo'",
 	}, {
 		input:  "show charset",
 		output: "show charset",
 	}, {
 		input:  "show charset like '%foo'",
-		output: "show charset",
+		output: "show charset like '%foo'",
 	}, {
 		input:  "show collation",
 		output: "show collation",
